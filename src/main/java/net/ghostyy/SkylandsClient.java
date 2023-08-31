355baf8f29c4942103e1d553109913b87407ccdf
Ghostyy06 <jkoproski06@gmail.com>
2023-08-31 13:39:10 -0400

    init

---
+++ src/main/java/net/ghostyy/SkylandsClient.java
@@ -0,0 +1,10 @@
+package net.ghostyy;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SkylandsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
